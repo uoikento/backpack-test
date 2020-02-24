b252cfb8a1cfae687e3c83f8d5995d5464262c20
kento.uoi <fishperson19@gmail.com>
2020-02-25 01:44:16 +0900

    Initial Commit

---
+++ backpack-test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  backpack-test
+//
+//  Created by 魚井　健人 on 2020/02/25.
+//  Copyright © 2020 kennto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ backpack-testTests/backpack_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  backpack_testTests.swift
+//  backpack-testTests
+//
+//  Created by 魚井　健人 on 2020/02/25.
+//  Copyright © 2020 kennto. All rights reserved.
+//
+
+import XCTest
+@testable import backpack_test
+
+class backpack_testTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
